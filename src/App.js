3ed253af665344710bc7e938c368056dbcc5f8a7
Tresor Sawasawa <tresorsawasawa@gmail.com>
2022-11-07 19:10:43 +0200

    Added logic code on the Reasons Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Hero from './components/Hero';
 import Programs from './components/Programs';
+import Reasons from './components/Reasons';
 
 const App = () => (
   <div className="App">
     <Hero />
     <Programs />
+    <Reasons />
   </div>
 );
 
