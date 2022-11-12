19471c5f8577baaf3b105473a29242701f77ecc5
Tresor Sawasawa <tresorsawasawa@gmail.com>
2022-11-12 16:05:37 +0200

    Add all code in the Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import GitHub from '../assets/images/github.png';
+import Instagram from '../assets/images/instagram.png';
+import Linkedin from '../assets/images/linkedin.png';
+import Logo from '../assets/images/logo.png';
+
+const Footer = () => (
+  <footer id="footer" className="footer">
+    <hr className="horizontal" />
+    <div className="row">
+      <div className="logo_footer">
+        <a href="https://github.com">
+          <img src={Logo} alt="Logo" />
+        </a>
+      </div>
+
+      <ul className="footer_icons">
+        <li className="icon-item">
+          <a href="https://github.com"><img src={GitHub} alt="GitHub Icon" /></a>
+        </li>
+        <li className="icon-item">
+          <a href="https://www.facebook.com/"><img src={Instagram} alt="Instagram Icon" /></a>
+        </li>
+        <li className="icon-item">
+          <a href="https://www.linkedin.com/feed/"><img src={Linkedin} alt="Linkedin Icon" /></a>
+        </li>
+      </ul>
+    </div>
+  </footer>
+);
+
+export default Footer;
