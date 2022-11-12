c249ee05f30cdc839949b42f0fe0f6b3282daf80
Tresor Sawasawa <tresorsawasawa@gmail.com>
2022-11-12 19:29:46 +0200

    Add animations to different sections

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,87 +1,119 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import Header from './Header';
 import HeroImage from '../assets/images/hero_image.png';
 import HeroImageBack from '../assets/images/hero_image_back.png';
 import Heart from '../assets/images/heart.png';
 import Calories from '../assets/images/calories.png';
 
-const Hero = () => (
-  <section className="hero-container">
-    <div className="bur hero-blur" />
-    <div className="left-hero">
-      <Header />
+const Hero = () => {
+  const transition = {
+    type: 'spring',
+    duration: 3,
+  };
 
-      <div className="the-best-ad">
-        <div className="background" />
-        <span>The best fitness club in the town</span>
+  return (
+    <section className="hero-container">
+      <div className="bur hero-blur" />
+      <div className="left-hero">
+        <Header />
+
+        <div className="the-best-ad">
+          <motion.div
+            initial={{ left: '283px' }}
+            whileInView={{ left: '8px' }}
+            transition={{ ...transition, type: 'tween' }}
+            className="background"
+          />
+          <span>The best fitness club in the town</span>
+        </div>
+
+        <div className="hero-text">
+          <div className="text">
+            <span className="stroke-text">Shape </span>
+            <span>Your</span>
+          </div>
+
+          <div>
+            <span>Ideal Body</span>
+          </div>
+
+          <div className="text-description">
+            <p>
+              In here we will help you to shape and build your ideal body and
+              live up your life to fullest.
+            </p>
+          </div>
+        </div>
+
+        <div className="figures">
+          <div className="block-fig fig-1">
+            <span>+140</span>
+            <span>expert coaches</span>
+          </div>
+
+          <div className="block-fig fig-2">
+            <span>+978</span>
+            <span>members joined</span>
+          </div>
+
+          <div className="block-fig fig-3">
+            <span>+50</span>
+            <span>fitness programs</span>
+          </div>
+        </div>
+
+        <div className="hero-buttons">
+          <button type="button" className="btn">
+            Get Started
+          </button>
+          <button type="button" className="btn">
+            Learn More
+          </button>
+        </div>
       </div>
-
-      <div className="hero-text">
-        <div className="text">
-          <span className="stroke-text">Shape </span>
-          <span>Your</span>
-        </div>
-
-        <div>
-          <span>Ideal Body</span>
-        </div>
-
-        <div className="text-description">
-          <p>
-            In here we will help you to shape and build your ideal body and live
-            up your life to fullest.
-          </p>
-        </div>
-      </div>
-
-      <div className="figures">
-        <div className="block-fig fig-1">
-          <span>+140</span>
-          <span>expert coaches</span>
-        </div>
-
-        <div className="block-fig fig-2">
-          <span>+978</span>
-          <span>members joined</span>
-        </div>
-
-        <div className="block-fig fig-3">
-          <span>+50</span>
-          <span>fitness programs</span>
-        </div>
-      </div>
-
-      <div className="hero-buttons">
+      <div className="right-hero">
         <button type="button" className="btn">
-          Get Started
+          Join Now
         </button>
-        <button type="button" className="btn">
-          Learn More
-        </button>
+
+        <motion.div
+          initial={{ right: '-1rem' }}
+          whileInView={{ left: '4rem' }}
+          transition={transition}
+          className="heart-rate"
+        >
+          <img src={Heart} className="love_img" alt="Heart pic" />
+
+          <span>Heart Rate</span>
+          <span>116 bpm</span>
+        </motion.div>
+
+        <img src={HeroImage} className="hero_image" alt="Hero_image" />
+        <motion.img
+          initial={{ right: '11rem' }}
+          whileInView={{ right: '20rem' }}
+          transition={transition}
+          src={HeroImageBack}
+          className="hero_image_back"
+          alt="Hero_image_back"
+        />
+
+        <motion.div
+          initial={{ right: '37rem' }}
+          whileInView={{ right: '25rem' }}
+          transition={transition}
+          className="calories"
+        >
+          <img src={Calories} className="calories_img" alt="calories_img" />
+          <div className="calories_text">
+            <span className="ssa">Calories Burned</span>
+            <span>220kcal</span>
+          </div>
+        </motion.div>
       </div>
-    </div>
-    <div className="right-hero">
-      <button type="button" className="btn">Join Now</button>
-
-      <div className="heart-rate">
-        <img src={Heart} className="love_img" alt="Heart pic" />
-
-        <span>Heart Rate</span>
-        <span>116 bpm</span>
-      </div>
-
-      <img src={HeroImage} className="hero_image" alt="Hero_image" />
-      <img src={HeroImageBack} className="hero_image_back" alt="Hero_image_back" />
-
-      <div className="calories">
-        <img src={Calories} className="calories_img" alt="calories_img" />
-        <div className="calories_text">
-          <span className="ssa">Calories Burned</span>
-          <span>220kcal</span>
-        </div>
-      </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default Hero;
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { motion } from 'framer-motion';
 import testimonialsData from '../data/testimonialsData';
 import leftArrow from '../assets/images/leftArrow.png';
 import rightArrow from '../assets/images/rightArrow.png';
@@ -6,6 +7,46 @@ import rightArrow from '../assets/images/rightArrow.png';
 const Testimonials = () => {
   const [selected, setSelected] = useState(0);
   const tDataLength = testimonialsData.length;
+
+  // Animations variables start
+  const transition = {
+    type: 'spring',
+    duration: 3,
+  };
+
+  const initial = {
+    opacity: 0,
+    x: -100,
+  };
+
+  const whileInView = {
+    opacity: 1,
+    x: 0,
+  };
+
+  const initial1 = {
+    opacity: 0,
+    x: 100,
+  };
+
+  const whileInView1 = {
+    opacity: 1,
+    x: 0,
+  };
+
+  const animTestimonialImg = {
+    initial: { opacity: 0, x: 100 },
+    animate: { opacity: 1, x: 0 },
+    exit: { opacity: 0, x: -100 },
+  };
+
+  const reviewAnimation = {
+    initial: { opacity: 0, x: -100 },
+    animate: { opacity: 1, x: 0 },
+    exit: { opacity: 0, x: 100 },
+  };
+
+  // Animations variables end
 
   const handleLeftArrow = () => {
     if (selected === 0) {
@@ -16,7 +57,7 @@ const Testimonials = () => {
   };
 
   const handleRightArrow = () => {
-    if (selected === (tDataLength - 1)) {
+    if (selected === tDataLength - 1) {
       setSelected(0);
     } else {
       setSelected((prev) => prev + 1);
@@ -29,7 +70,15 @@ const Testimonials = () => {
         <span>Testimonials </span>
         <span className="stroke-text">What they </span>
         <span>say about us</span>
-        <p>{testimonialsData[selected].review}</p>
+        <motion.p
+          key={selected}
+          initial={reviewAnimation.initial}
+          animate={reviewAnimation.animate}
+          exit={reviewAnimation.exit}
+          transition={transition}
+        >
+          {testimonialsData[selected].review}
+        </motion.p>
         <div className="name-status">
           <span className="name">{testimonialsData[selected].name}</span>
           {' - '}
@@ -38,9 +87,24 @@ const Testimonials = () => {
       </div>
 
       <div className="right-t">
-        <div className="border" />
-        <div className="background" />
-        <img
+        <motion.div
+          initial={initial}
+          whileInView={whileInView}
+          transition={transition}
+          className="border"
+        />
+        <motion.div
+          initial={initial1}
+          whileInView={whileInView1}
+          transition={{ ...transition, duration: 2 }}
+          className="background"
+        />
+        <motion.img
+          key={selected}
+          initial={animTestimonialImg.initial}
+          animate={animTestimonialImg.animate}
+          exit={animTestimonialImg.exit}
+          transition={transition}
           src={testimonialsData[selected].image}
           className="reviewer_image"
           alt="reviewer_image"
